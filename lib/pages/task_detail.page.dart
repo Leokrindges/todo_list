63083e669b207446a4e87146ecac5fafe3995d41
Leonardo Krindges <krindgesleonardo3@gmail.com>
2025-08-09 10:56:03 -0300

    feat: Add validation to the task details form and refactor input fields

--- lib/pages/task_detail.page.dart
+++ lib/pages/task_detail.page.dart
@@ -13,6 +13,7 @@ class _TaskDetailPageState extends State<TaskDetailPage> {
   final titleController = TextEditingController();
   final descriptionController = TextEditingController();
   late bool isImportant;
+  final formKey = GlobalKey<FormState>();
 
   @override
   void initState() {
@@ -23,9 +24,11 @@ class _TaskDetailPageState extends State<TaskDetailPage> {
   }
 
   saveTask() {
+    if (!formKey.currentState!.validate()) {
+      return;
+    }
     final updatedTask = widget.task;
     updatedTask.important = isImportant;
-    print('Tarefa importante: ${updatedTask.important}');
     updatedTask.title = titleController.text;
     updatedTask.description = descriptionController.text.isEmpty
         ? null
@@ -51,39 +54,48 @@ class _TaskDetailPageState extends State<TaskDetailPage> {
       ),
       body: Padding(
         padding: EdgeInsets.all(12.0),
-        child: Column(
-          children: [
-            TextField(
-              controller: titleController,
-              decoration: InputDecoration(
-                labelText: 'Título',
-                border: OutlineInputBorder(),
-              ),
-            ),
-            SizedBox(height: 20),
-            TextField(
-              controller: descriptionController,
-              maxLines: 5,
-              decoration: InputDecoration(
-                labelText: 'Descrição',
-                border: OutlineInputBorder(),
-              ),
-            ),
-            SizedBox(height: 40),
-            TextButton(onPressed: saveTask, child: Text('Salvar Tarefa')),
-            Spacer(),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text('Criada Sex, 24 de mar'),
-                IconButton(
-                  onPressed: () {},
-                  icon: Icon(Icons.delete_outline),
-                  iconSize: 28,
+        child: Form(
+          key: formKey,
+          child: Column(
+            children: [
+              TextFormField(
+                controller: titleController,
+                decoration: InputDecoration(
+                  labelText: 'Título',
+                  border: OutlineInputBorder(),
                 ),
-              ],
-            ),
-          ],
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Campo obrigatório';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 20),
+              TextField(
+                controller: descriptionController,
+                maxLines: 5,
+                decoration: InputDecoration(
+                  labelText: 'Descrição',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              SizedBox(height: 40),
+              TextButton(onPressed: saveTask, child: Text('Salvar Tarefa')),
+              Spacer(),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text('Criada Sex, 24 de mar'),
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.delete_outline),
+                    iconSize: 28,
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
